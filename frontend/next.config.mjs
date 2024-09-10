0ba4ca0dddc664e1debe2a91c65737b5eda6b066
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2024-09-10 13:57:26 +0530

    resolve middleware issues

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -26,7 +26,6 @@ const nextConfig = {
   },
   async rewrites() {
     return [
-      { source: '/api/:path*', destination: `${process.env.BACKEND_URL}/api/:path*` },
       { source: '/auth/:path*', destination: `${process.env.BACKEND_URL}/auth/:path*` }
     ];
   },
